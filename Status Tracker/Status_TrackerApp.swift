259f06e2e59cdc0eaf3ad17c120cd5d016898457
Guru Mahan <iosdeve2022@gmail.com>
2023-03-13 17:32:51 +0530

    Initial Commit

---
+++ Status Tracker/Status_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Status_TrackerApp.swift
+//  Status Tracker
+//
+//  Created by Guru Mahan on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Status_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
